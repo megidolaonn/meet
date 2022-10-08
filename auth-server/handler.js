31b944a2c1c7a13b273e9eab14c0f285d27c084b
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-10-08 18:49:46 -0400

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -72,9 +72,6 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
         body: JSON.stringify(err),
       };
     });
@@ -124,9 +121,6 @@ module.exports.getCalendarEvents = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
         body: JSON.stringify(err),
       };
     });
