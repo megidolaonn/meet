80da29750c3b7e970cefb0fbea1e07eb18b85780
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-10-17 02:37:31 -0400

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,7 +4,8 @@ class CitySearch extends Component {
 
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined
   }
 
   handleInputChanged = (event) => {
@@ -20,8 +21,11 @@ class CitySearch extends Component {
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+      query: suggestion,
+      showSuggestions: false
     });
+
+    this.props.updateEvents(suggestion);
   }
 
   render() {
@@ -30,18 +34,19 @@ class CitySearch extends Component {
         <input
           type="text"
           className="city"
-          placeholder="Search for a city"
+          placeholder="Search"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
-        <ul className="suggestions">
+        <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
               onClick={() => this.handleItemClicked(suggestion)}
             >{suggestion}</li>
           ))}
-          <li>
+          <li onClick={() => this.handleItemClicked("all")}>
             <b>See all cities</b>
           </li>
         </ul>
