0492b0ef66a10d1e03247dd4fd87b06a21382e48
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-11-27 22:15:33 -0500

    no chart label at 0 percent

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -34,7 +34,7 @@ const EventGenre = ({ events }) => {
           outerRadius={80}
           fill="8884d8"
           dataKey="value"
-          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+          label={({ name, percent }) => percent > 0 ? `${name} ${(percent * 100).toFixed(0)}%` : null}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index]} />
